3e6ec5b7b8dcd26a15a9703d28694ce6e34e5868
xloppi <hlopkovalesha@yandex.ru>
2021-07-25 13:46:33 +0300

    add component AboutMe

---
+++ src/components/AboutMe/AboutMe.js
@@ -0,0 +1,26 @@
+import "./AboutMe.css";
+import me_photo from '../../images/me.jpg';
+
+function AboutMe() {
+  return (
+    <section className="about-me">
+      <div className="about-me__container">
+        <h2 className="about-me__title main__section-title">Студент</h2>
+        <div className="about-me__content">
+          <div className="about-me__info">
+            <h3 className="about-me__name">Виталий</h3>
+            <p className="about-me__subtitle">Фронтенд-разработчик, 30 лет</p>
+            <p className="about-me__description">Я родился и живу в Саратове, закончил факультет экономики СГУ. У меня есть жена и дочь. Я люблю слушать музыку, а ещё увлекаюсь бегом. Недавно начал кодить. С 2015 года работал в компании «СКБ Контур». После того, как прошёл курс по веб&#8209;разработке, начал заниматься фриланс-заказами и ушёл с постоянной работы.</p>
+            <ul className="about-me__links">
+              <li className="about-me__link">Facebook</li>
+              <li className="about-me__link">Github</li>
+            </ul>
+          </div>
+          <img className="about-me__photo" src={me_photo} alt="фото"></img>
+        </div>
+      </div>
+    </section>
+  );
+}
+
+export default AboutMe;
--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -4,6 +4,7 @@ import Header from '../Header/Header';
 import Promo from '../Promo/Promo';
 import AboutProject from '../AboutProject/AboutProject';
 import Techs from '../Techs/Techs';
+import AboutMe from '../AboutMe/AboutMe';
 
 function Main() {
     return (
@@ -14,6 +15,7 @@ function Main() {
           <Promo />
           <AboutProject />
           <Techs />
+          <AboutMe />
         </main>
       </>
     );
