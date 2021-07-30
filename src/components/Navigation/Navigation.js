d771d002361fcd004c7b16b1e45869019e55591e
xloppi <hlopkovalesha@yandex.ru>
2021-07-30 17:55:21 +0300

    Refactor: Navigation

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,13 +1,31 @@
 import './Navigation.css';
+import account from '../../images/account-icon.svg';
 
 function Navigation() {
   return (
-    <nav className="navigation">
-      <ul className="navigation__list">
-        <a className="navigation__link-signup" href="#">Регистрация</a>
-        <a className="navigation__link-signin" href="#">Войти</a>
-      </ul>
-    </nav>
+    <div className="navigation">
+      <div className="navigation__burger-icon">
+        <span className="navigation__burger-icon-span"></span>
+        <span className="navigation__burger-icon-span"></span>
+        <span className="navigation__burger-icon-span"></span>
+      </div>
+{/*       <nav className="navigation__links">
+        <ul className="navigation__list">
+          <a className="navigation__link-signup" href="#">Регистрация</a>
+          <a className="navigation__link-signin" href="#">Войти</a>
+        </ul>
+      </nav> */}
+      <nav className="navigation__links">
+        <ul className="navigation__list">
+          <li className="navigation__item"><a className="navigation__link navigation__link_type_bold" href="#">Фильмы</a></li>
+          <li className="navigation__item"><a className="navigation__link" href="#">Сохранённые фильмы</a></li>
+          <li className="navigation__item navigation__link-account">
+            <img className="navigation__account-icon" src={account} />
+            <a className="navigation__link navigation__link_type_bold" href="#">Аккаунт</a>
+          </li>
+        </ul>
+      </nav>
+    </div>
   );
 }
 
