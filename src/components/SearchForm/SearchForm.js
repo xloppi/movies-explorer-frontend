9b3e0963ae2886ab0847dac7830b1fd91115527c
xloppi <hlopkovalesha@yandex.ru>
2021-08-05 17:04:32 +0300

    refactor search form

--- src/components/SearchForm/SearchForm.js
+++ src/components/SearchForm/SearchForm.js
@@ -1,18 +1,22 @@
-import './SearchForm.css';
-import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
+import "./SearchForm.css";
+import FilterCheckbox from "../FilterCheckbox/FilterCheckbox";
 
 function SearchForm() {
-    return (
-      <section className="search-form">
-        <div className="search-form__container">
-          <form className="search-form__form">
-            <input className="search-form__form-input" placeholder="Фильм"></input>
-            <button className="search-form__form-button">Поиск</button>
-          </form>
-          <FilterCheckbox />
-        </div>
-      </section>
-    );
+  return (
+    <section className="search-form">
+      <div className="search-form__container">
+        <form className="search-form__form">
+          <input
+            className="search-form__form-input"
+            placeholder="Фильм"
+            required
+          ></input>
+          <button className="search-form__form-button">Поиск</button>
+        </form>
+        <FilterCheckbox />
+      </div>
+    </section>
+  );
 }
 
 export default SearchForm;
